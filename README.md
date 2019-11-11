3a00287ead8ee50afe2ff16b87ba8fe6f013e455
Dean Iverson <diverson@vtti.vt.edu>
2019-11-11 16:35:55 -0700

    Init commit of code

---
+++ README.md
@@ -0,0 +1,3 @@
+### PPM - The Pouder Pit Map Generator
+
+This simple command line utility will (hopefully) generate a reasonable pit map for the Poudre FLL Qualifier.
---
+++ ppm.dart
@@ -0,0 +1,13 @@
+main() {
+  print('ppm usage: Hello there!');
+  printSvgHeader();
+  printSvgFooter();
+}
+
+void printSvgHeader() {
+  print('<svg>');
+}
+
+void printSvgFooter() {
+  print('</svg>');
+}
